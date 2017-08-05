a72c393725d95ea37d51812e86a8e5ff0f0a22dc
Ian Cleasby <icle6423@uni.sydney.edu.au>
2017-08-05 21:49:42 +1000

    Created Stategy header

---
+++ Patterns/strategy.hpp
@@ -0,0 +1,9 @@
+//
+// Created by penguinrage on 5/08/17.
+//
+
+#ifndef DESIGN_PATTERNS_STRATEGY_HPP
+#define DESIGN_PATTERNS_STRATEGY_HPP
+// TODO -  So tired, when I wake up I will write this design pattern
+
+#endif //DESIGN_PATTERNS_STRATEGY_HPP
